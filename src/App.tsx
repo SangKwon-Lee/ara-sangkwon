36050c0f186fb50a90a816f327de78ea5f6bfea0
SangKwon-Lee <sangkwon.lee@pxd.co.kr>
2023-08-01 13:39:32 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,137 +1,243 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
-//@ts-ignore
-const { kakao } = window;
+import {
+  Map,
+  MapMarker,
+  CustomOverlayMap,
+  MarkerClusterer,
+} from "react-kakao-maps-sdk";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Slider from "react-slick";
+import Drawer from "@mui/material/Drawer";
+const style = {
+  position: "absolute" as "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "70%",
+  maxWidth: 500,
+  boxShadow: 24,
+  borderRadius: 3,
+  outline: "none",
+};
 
-const data = [
-  {
-    latlng: { lat: 33.5563, lng: 126.79581 },
-    iwContent: (
-      <div className="wrapper">
-        <img className="images" src="/test.jpg" />
-        <div className="bottom">
-          <div className="titleWrap">
-            <div className="title"> 피맥과 함께</div>
-            <div className="day">+546</div>
-          </div>
-          <div className="place">맥파이</div>
-          <div className="date"> 23.07.31</div>
-        </div>
-      </div>
-    ),
-  },
-  {
-    latlng: { lat: 33.5563, lng: 126.79881 },
-    iwContent: [
-      '<div class="wrapper" >',
-      '<img  class="images" src="/test.jpg"/>',
-      '<div class="bottom">',
-      '<div class="titleWrap">',
-      '<div class="title"> 피맥과 함께</div>',
-      '<div class="day">+546</div>',
-      "</div>",
-      '<div class="place">맥파이</div>',
-      '<div class="dateWrap">',
-      '<div class="date"> 23.07.31</div>',
-      '<div class="more">더보기</div>',
-      "</div>",
-      "</div>",
-      "</div>",
-    ].join(""),
-  },
-];
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 function App() {
-  // const [clickedOverlay, setClickedOverlay] = useState<any>();
-  // const [openModal, setOpenModal] = useState(false);
-  // useEffect(() => {
-  //   var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-  //     mapOption = {
-  //       center: new kakao.maps.LatLng(37.4862618, 127.1222903), // 지도의 중심좌표
-  //       level: 3, // 지도의 확대 레벨
-  //     };
-
-  //   var imageSrc = "/heart.svg", // 마커이미지의 주소입니다
-  //     imageSize = new kakao.maps.Size(25, 25), // 마커이미지의 크기입니다
-  //     imageOption = { offset: new kakao.maps.Point(0, 0) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-  //   // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-  //   var markerImage = new kakao.maps.MarkerImage(
-  //     imageSrc,
-  //     imageSize,
-  //     imageOption
-  //   );
-
-  //   // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-  //   var map = new kakao.maps.Map(mapContainer, mapOption);
-
-  //   data.map((item) => {
-  //     const markerPosition = new kakao.maps.LatLng(
-  //       item.latitude,
-  //       item.longitude
-  //     );
-  //     const marker = new kakao.maps.Marker({
-  //       position: markerPosition,
-  //       image: markerImage,
-  //     });
-
-  //     var content = document.createElement("div");
-  //     content.onclick = function () {
-  //       customOverlay.setMap(null);
-  //     };
-  //     content.classList.add("close");
-  //     var more = document.createElement("div");
-  //     more.classList.add("more");
-  //     more.innerHTML = "더보기";
-  //     more.onclick = function () {
-  //       setOpenModal(true);
-  //     };
-  //     var stringToHTML = function (str: any) {
-  //       var dom = document.createElement("div");
-  //       dom.innerHTML = str;
-  //       return dom;
-  //     };
-  //     const tags = stringToHTML(item.iwContent);
-  //     tags.appendChild(content);
-  //     tags.appendChild(more);
-  //     var customOverlay = new kakao.maps.CustomOverlay({
-  //       position: marker.getPosition(),
-  //       content: item.iwContent + content,
-  //       yAnchor: 1.1,
-  //       xAnchor: 0.4,
-  //       removable: true,
-  //     });
-  //     customOverlay.setContent(tags);
-  //     marker.setMap(map);
-  //     handleClickOverlay(marker, customOverlay, map);
-  //   });
-  // }, []);
-
-  const handleClickOverlay = (marker: any, customOverlay: any, map: any) => {
-    kakao.maps.event.addListener(marker, "click", function () {
-      customOverlay.setMap(map);
-    });
-  };
+  const [center, setCenter] = useState({
+    lat: 33.5563,
+    lng: 126.79881,
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [overlayPosition, setOverlayPosition] = useState({ lat: 0, lng: 0 });
+  const [heartPosition, setHeartPosition] = useState({ lat: 0, lng: 0 });
+  const [open, setOpen] = useState(false);
+  const data = [
+    {
+      lat: 33.5563,
+      lng: 126.79581,
+    },
+    {
+      lat: 33.5563,
+      lng: 126.79881,
+    },
+  ];
 
   return (
     <>
+      <header className="header">
+        <img
+          className="menu"
+          src="/menu.svg"
+          alt="menu"
+          onClick={() => {
+            setOverlayPosition({
+              lat: 0,
+              lng: 0,
+            });
+            setHeartPosition({
+              lat: 0,
+              lng: 0,
+            });
+            setOpen(true);
+          }}
+        />
+        <h1 className="header-title">ara-sangkwon</h1>
+      </header>
       <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
+        center={center}
+        isPanto={true}
         style={{ width: "100%", height: "100vh" }}
+        level={3}
       >
-        {data.map((position) => (
-          <MapMarker
-            key={`${position.latlng.lat}-${position.latlng.lng}`}
-            position={position.latlng}
-            image={{ src: "/heart.svg", size: { width: 25, height: 25 } }}
-          ></MapMarker>
-        ))}
-        {data.map((item) => (
-          <CustomOverlayMap position={item.latlng}>
-            {item.iwContent}
-          </CustomOverlayMap>
-        ))}
+        <MarkerClusterer minLevel={3} averageCenter={true}>
+          {data.map((position, index) => (
+            <MapMarker
+              title={String(index)}
+              onClick={() => {
+                setCenter({ lat: position.lat, lng: position.lng });
+                setOverlayPosition({
+                  lat: position.lat,
+                  lng: position.lng,
+                });
+              }}
+              key={`${position.lat}-${position.lng}`}
+              position={{ lat: position.lat, lng: position.lng }}
+              image={{
+                src:
+                  (position.lat === heartPosition.lat &&
+                    position.lng === heartPosition.lng) ||
+                  (overlayPosition.lat === position.lat &&
+                    overlayPosition.lng === position.lng)
+                    ? `/heart_pink.svg`
+                    : `/heart_black.svg`,
+                size: { width: 25, height: 25 },
+              }}
+              onMouseOver={() =>
+                setHeartPosition({
+                  lat: position.lat,
+                  lng: position.lng,
+                })
+              }
+              onMouseOut={() =>
+                setHeartPosition({
+                  lat: 0,
+                  lng: 0,
+                })
+              }
+            ></MapMarker>
+          ))}
+        </MarkerClusterer>
+        {data.map(
+          (item, index) =>
+            overlayPosition.lat === item.lat &&
+            overlayPosition.lng === item.lng && (
+              <CustomOverlayMap
+                position={{ lat: item.lat, lng: item.lng }}
+                yAnchor={1.15}
+                key={index}
+              >
+                <div className="wrapper">
+                  <img
+                    className="images"
+                    alt="img"
+                    src="/test.jpg"
+                    onClick={() => setIsOpen(true)}
+                  />
+                  <div className="bottom">
+                    <div className="titleWrap">
+                      <div className="title"> 피맥과 함께</div>
+                      <div className="day">+546</div>
+                    </div>
+                    <div className="place">맥파이</div>
+                    <div className="date"> 23.07.31</div>
+                  </div>
+                </div>
+              </CustomOverlayMap>
+            )
+        )}
       </Map>
+
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="modal-wrap">
+            <Slider {...settings}>
+              <div>
+                <img className="modal-img" alt="img" src="/test.jpg" />
+              </div>
+              <div>
+                <img className="modal-img" alt="img" src="/test.jpg" />
+              </div>
+            </Slider>
+            <div className="modal-bottom">
+              <div className="modal-title">피맥과 함께</div>
+              <div className="modal-place">맥파이</div>
+              <div className="modal-desc">
+                피맥과 함께피맥과 함께피맥과 함께피맥과 함께피맥과 함께 피맥과
+                함께피맥과 함께피맥과 함께피맥과 함께피맥과 함께
+              </div>
+              <div className="modal-tittle-wrap">
+                <div className="modal-date">23.07.23</div>
+                <div className="modal-day">+546</div>
+              </div>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+
+      <Drawer
+        sx={{ width: "100%" }}
+        anchor={"left"}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div className="drawer-wrap">
+          <div
+            className="drawer-item"
+            onClick={() => {
+              setCenter({ lat: 33.5563, lng: 126.79581 });
+              setOpen(false);
+              setOverlayPosition({
+                lat: 33.5563,
+                lng: 126.79581,
+              });
+              setHeartPosition({
+                lat: 33.5563,
+                lng: 126.79581,
+              });
+            }}
+          >
+            <div className="drawer-left-wrap">
+              546일
+              <div className="drawer-left">
+                <div className="drawer-title">피맥과 함께</div>
+                <div className="drawer-place">맥파이</div>
+                <div className="drawer-date">23.07.23</div>
+              </div>
+            </div>
+            <img className="drawer-img" src="/test.jpg" alt="img" />
+          </div>
+          <div
+            className="drawer-item"
+            onClick={() => {
+              setCenter({ lat: 33.5563, lng: 126.79881 });
+              setOpen(false);
+            }}
+          >
+            <div className="drawer-left-wrap">
+              546일
+              <div className="drawer-left">
+                <div className="drawer-title">피맥과 함께</div>
+                <div className="drawer-place">맥파이</div>
+                <div className="drawer-date">23.07.23</div>
+              </div>
+            </div>
+            <img className="drawer-img" src="/test.jpg" alt="img" />
+          </div>{" "}
+          <div className="drawer-item">
+            <div className="drawer-left-wrap">
+              546일
+              <div className="drawer-left">
+                <div className="drawer-title">피맥과 함께</div>
+                <div className="drawer-place">맥파이</div>
+                <div className="drawer-date">23.07.23</div>
+              </div>
+            </div>
+            <img className="drawer-img" src="/test.jpg" alt="img" />
+          </div>
+        </div>
+      </Drawer>
     </>
   );
 }
